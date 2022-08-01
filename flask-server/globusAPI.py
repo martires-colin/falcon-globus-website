a35c1774b36d1badf6a28733f4428d413a5f7b92
Colin <cmartires@nevada.unr.edu>
2022-08-01 15:03:46 -0700

    displays files kinda

--- flask-server/globusAPI.py
+++ flask-server/globusAPI.py
@@ -36,7 +36,6 @@ app.secret_key = '\xaa\xa7\xf5\xf9\xd3G:\x17\x0f\xd7\x9c\xee\x1c\x9a\xb4\x80\xb4
 
 CLIENT_ID = "fe1fe34a-b528-4c7d-8708-5ebcfc53cc5a"
 CLIENT_SECRET = "CQyDAvpU1oGp36Hs7omJZRFTzbLtPFOmje6yFCnmIGY="
-# REDIRECT_URI should probably be homepage
 REDIRECT_URI = "http://localhost:5000/code_page"
 SCOPES = [
             GroupsScopes.view_my_groups_and_memberships,
@@ -46,20 +45,17 @@ SCOPES = [
             AuthScopes.profile
         ]
 
-
 client = globus_sdk.ConfidentialAppAuthClient(CLIENT_ID, CLIENT_SECRET)
 
 @app.route("/")
 @app.route("/home")
 def home():
     client.oauth2_start_flow(redirect_uri=REDIRECT_URI, requested_scopes=SCOPES)
-
     return render_template(
         'home.html',
         auth_url=client.oauth2_get_authorize_url()
     )
 
-# can change "/code_page" later
 @app.route("/code_page")
 def code_page():
     auth_code = request.args.get('code')
@@ -74,52 +70,33 @@ def code_page():
 @app.route("/dashboard")
 def dashboard():
 
+    authToken = session["globus_auth_token"]
+    transferToken = session["globus_transfer_token"]
+    groupsToken = session["globus_groups_token"]
     if "globus_auth_token" and "globus_transfer_token" and "globus_groups_token" in session:
-        return session["globus_auth_token"]
+        
+        # change epID and path to variables retireved from search?
+        files = getFiles('ceea5ca0-89a9-11e7-a97f-22000a92523b', '/~/')
+        
+        
+        
+        
+        return render_template('dashboard.html', files=files)
     else:
-        return redirect(url_for("home"))
+        return redirect(url_for('home'))
 
 
 
-
-
-# RETRIEVE GROUP INFORMATION
-@app.route("/group_info/<globus_groups_token>")
-def getGroupInfo(globus_groups_token):
-    groups_client = globus_sdk.GroupsClient(
-        authorizer=globus_sdk.AccessTokenAuthorizer(globus_groups_token)
-    )
-    group_data = []
-    groups = groups_client.get_my_groups()
-    for group in groups:
-        group_data.append(group)
-
-    return jsonify({"groups": group_data})
 
 # RETRIEVE USER INFO
-@app.route("/user_info/<globus_auth_token>")
-def getUserInfo(globus_auth_token):
+@app.route("/user_info")
+def user_info():
+    authToken = session["globus_auth_token"]
     auth_client = globus_sdk.AuthClient(
-        authorizer=globus_sdk.AccessTokenAuthorizer(globus_auth_token)
+        authorizer=globus_sdk.AccessTokenAuthorizer(authToken)
     )
-    user_info = auth_client.oauth2_userinfo()
 
-    identities = []
-    for identity in user_info["identity_set"]:
-        identities.append(identity["identity_provider_display_name"])
-    id_list = ', '.join(identities)
-
-    return {
-        'Sub': user_info["sub"],
-        'Organization': user_info["organization"],
-        'Name':user_info["name"],
-        'Username': user_info["preferred_username"],
-        'IDP': user_info["identity_provider"],
-        'IDP Display Name': user_info["identity_provider_display_name"],
-        'Email': user_info["email"],
-        'Last Authentication': user_info["last_authentication"],
-        'Identity Set': id_list
-    }
+    return render_template('userInfo.html', user_info=auth_client.oauth2_userinfo())
 
 
 # SEARCH COLLECTIONS
@@ -141,66 +118,86 @@ def searchCollections(tc):
 # path = '/usr/include/'
 
 # RETRIEVE USER FILES
-def getFiles(tc):
-    ep_id = input("\nEnter endpoint ID: ").strip()
-    path = input("Specify Path: ").strip()
-    print('\n----------------FILE INFORMATION-----------------\n')
-    for entry in tc.operation_ls(ep_id, path=path, orderby=["name"]):
-        print(
-            f'File Name: {entry["name"]}\n' \
-            f'Last Modified: {entry["last_modified"]}\n' \
-            f'Size: {entry["size"]} bytes\n' \
-            f'Permissions: {entry["permissions"]}\n' \
-            f'Type: {entry["type"]}\n' \
-            f'User: {entry["user"]}\n' \
-            f'Group: {entry["group"]}\n' \
-            f'DATA_TYPE: {entry["DATA_TYPE"]}\n' \
-            f'link_group: {entry["link_group"]}\n' \
-            f'link_last_modified: {entry["link_last_modified"]}\n' \
-            f'link_size: {entry["link_size"]}\n' \
-            f'link_target: {entry["link_target"]}\n' \
-            f'link_user: {entry["link_user"]}\n'
-        )
+def getFiles(epID, path):
+    transferToken = session["globus_transfer_token"]
+    transfer_client = globus_sdk.TransferClient(
+        authorizer=globus_sdk.AccessTokenAuthorizer(transferToken)
+    )
+
+    # ep_id = input("\nEnter endpoint ID: ").strip()
+    # path = input("Specify Path: ").strip()
+    # print('\n----------------FILE INFORMATION-----------------\n')
+    # for entry in transfer_client.operation_ls(epID, path=path, orderby=["name"]):
+    #     print(
+    #         f'File Name: {entry["name"]}\n' \
+    #         f'Last Modified: {entry["last_modified"]}\n' \
+    #         f'Size: {entry["size"]} bytes\n' \
+    #         f'Permissions: {entry["permissions"]}\n' \
+    #         f'Type: {entry["type"]}\n' \
+    #         f'User: {entry["user"]}\n' \
+    #         f'Group: {entry["group"]}\n' \
+    #         f'DATA_TYPE: {entry["DATA_TYPE"]}\n' \
+    #         f'link_group: {entry["link_group"]}\n' \
+    #         f'link_last_modified: {entry["link_last_modified"]}\n' \
+    #         f'link_size: {entry["link_size"]}\n' \
+    #         f'link_target: {entry["link_target"]}\n' \
+    #         f'link_user: {entry["link_user"]}\n'
+    #     )
+
+    return transfer_client.operation_ls(epID, path=path, orderby=["name"])
 
 
 # TRANSFER FILES
-def transferFiles(tc):
+# def transferFiles(tc):
     
-    # sourceEP = input("\nEnter source end point: ").strip()
-    # destEP = input("Enter destination end point: ").strip()
+#     # sourceEP = input("\nEnter source end point: ").strip()
+#     # destEP = input("Enter destination end point: ").strip()
     
-    sourceEP = 'ceea5ca0-89a9-11e7-a97f-22000a92523b'
-    destEP = 'b256c034-1578-11eb-893e-0a5521ff3f4b'
+#     sourceEP = 'ceea5ca0-89a9-11e7-a97f-22000a92523b'
+#     destEP = 'b256c034-1578-11eb-893e-0a5521ff3f4b'
 
-    sourceFilePath = input("Enter source file path: ").strip()
-    destFilePath = input("Enter destination file path: ").strip()
+#     sourceFilePath = input("Enter source file path: ").strip()
+#     destFilePath = input("Enter destination file path: ").strip()
 
-    tdata = globus_sdk.TransferData(tc, sourceEP, destEP)
-    tdata.add_item(sourceFilePath, destFilePath)
-    transfer_result = tc.submit_transfer(tdata)
+#     tdata = globus_sdk.TransferData(tc, sourceEP, destEP)
+#     tdata.add_item(sourceFilePath, destFilePath)
+#     transfer_result = tc.submit_transfer(tdata)
 
-    taskID = transfer_result["task_id"]
+#     taskID = transfer_result["task_id"]
 
-    tc.task_wait(taskID, timeout=60, polling_interval=1)
-    # while not tc.task_wait(taskID, timeout=1, polling_interval=1):
-    #     print('.', end="")
+#     tc.task_wait(taskID, timeout=60, polling_interval=1)
+#     # while not tc.task_wait(taskID, timeout=1, polling_interval=1):
+#     #     print('.', end="")
 
-    taskStatus = tc.get_task(taskID)
+#     taskStatus = tc.get_task(taskID)
 
-    print(f'\nTRANSFER RESULT:\n{transfer_result}')
-    print(f'\nTASK STATUS:\n{taskStatus}')
+#     print(f'\nTRANSFER RESULT:\n{transfer_result}')
+#     print(f'\nTASK STATUS:\n{taskStatus}')
 
 
-def displayMenu():
-    print(
-        '\n---------------Menu OPTIONS---------------\n'
-        '1: Display User Info\n' \
-        '2: Display User Group Info\n' \
-        '3: Search Collections\n' \
-        '4: List Collection Files\n' \
-        '5: Transfer Files\n' \
-        '6: Exit Program\n'
-    )
+# def displayMenu():
+#     print(
+#         '\n---------------Menu OPTIONS---------------\n'
+#         '1: Display User Info\n' \
+#         '2: Display User Group Info\n' \
+#         '3: Search Collections\n' \
+#         '4: List Collection Files\n' \
+#         '5: Transfer Files\n' \
+#         '6: Exit Program\n'
+#     )
+
+# # RETRIEVE GROUP INFORMATION
+# # @app.route("/group_info/<globus_groups_token>")
+# def getGroupInfo(globus_groups_token):
+#     groups_client = globus_sdk.GroupsClient(
+#         authorizer=globus_sdk.AccessTokenAuthorizer(globus_groups_token)
+#     )
+#     group_data = []
+#     groups = groups_client.get_my_groups()
+#     for group in groups:
+#         group_data.append(group)
+
+#     return {"groups": group_data}
 
 # def main():
 
